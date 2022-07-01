811a54596c3baf8cf58ed7c13f5e1bb108f20dd2
Tasadduq Ali <tasduqali2@gmail.com>
2022-07-01 14:23:25 +0500

    fixing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const app = express();
 
-// const aristotleRoutes = require("./Routes/Aristotle-routes");
+const aristotleRoutes = require("./Routes/Aristotle-routes");
 const finiksRoutes = require("./Routes/Finiks-routes");
 const phonebankRoutes = require("./Routes/Phonebank-routes");
 const canvassingRoutes = require("./Routes/Canvassing-routes");
@@ -15,11 +16,31 @@ const tagsRoutes = require("./Routes/Tags-routes");
 const PORT = process.env.PORT || 3001;
 const { db } = require("./Config/config");
 
-const app = express();
-
 // app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requseted-With, Content-Type, Accept , Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+
+  next();
+});
+
+app.use("/api/aristotle", aristotleRoutes);
+app.use("/api/finiks", finiksRoutes);
+app.use("/api/phonebank", phonebankRoutes);
+app.use("/api/canvassing", canvassingRoutes);
+app.use("/api/campaign", campaignRoutes);
+app.use("/api/teammember", teammemberRoutes);
+app.use("/api/clients", clientsRoutes);
+app.use("/api/script", scriptRoutes);
+app.use("/api/survey", surveyRoutes);
+app.use("/api/tags", tagsRoutes);
 
 // let db;
 mongoose
@@ -39,28 +60,6 @@ mongoose
   .catch((err) => {
     console.log("error", err.message);
   });
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requseted-With, Content-Type, Accept , Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-
-  next();
-});
-
-// app.use("/api/aristotle", aristotleRoutes);
-app.use("/api/finiks", finiksRoutes);
-app.use("/api/phonebank", phonebankRoutes);
-app.use("/api/canvassing", canvassingRoutes);
-app.use("/api/campaign", campaignRoutes);
-app.use("/api/teammember", teammemberRoutes);
-app.use("/api/clients", clientsRoutes);
-app.use("/api/script", scriptRoutes);
-app.use("/api/survey", surveyRoutes);
-app.use("/api/tags", tagsRoutes);
 
 app.listen(PORT, () => {
   console.log("listening on " + PORT);
